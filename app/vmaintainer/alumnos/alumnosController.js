24f3ca921c46d1fa52ffa9c73425063d941f4d1c
cmarint@gmail.com <cmarint@gmail.com>
2017-08-10 15:52:49 -0400

    Factoria

--- app/vmaintainer/alumnos/alumnosController.js
+++ app/vmaintainer/alumnos/alumnosController.js
@@ -1,112 +1,152 @@
-app.factory('alumnosFact', function($http, $q, CONFIG   ){
-  return{
-        get: function()
+app.factory('apiAlumnoFactory', function($http, $q, CONFIG){
+    return {
+        getTodos: function()
         {
-            var deferred;
             deferred = $q.defer();
             $http({
                 method: 'GET',
                 skipAuthorization: true,
-                url: CONFIG.APIJSON + '/users'
-            })
-            .then(function(res)
-            {
+                url: CONFIG.APISOSTOSBE + '/alumno/get'
+            }).then(function(res) {
                 deferred.resolve(res);
+            }).then(function(error){
+                deferred.reject(error);
             })
-            .then(function(error)
-            {
+            return deferred.promise;
+        },
+        getIns: function()
+        {
+            deferred = $q.defer();
+            $http({
+                method: 'GET',
+                skipAuthorization: true,
+                url: CONFIG.APISOSTOSBE + '/institucion/get'
+            }).then(function(res) {
+                deferred.resolve(res);
+            }).then(function(error){
+                deferred.reject(error);
+            })
+            return deferred.promise;
+        },
+        setAsi: function(registro)
+        {
+            var regjson = angular.toJson(registro);
+            deferred = $q.defer();
+            $http({
+                method: 'POST',
+                //skipAuthorization: true,
+                url: CONFIG.APISOSTOSBE + '/alumno/upd',
+                data: regjson,
+            }).then(function(res) {
+                deferred.resolve(res);
+            }).then(function(error){
+                deferred.reject(error);
+            })
+            return deferred.promise;
+        },
+        addAsi: function(registro)
+        {
+            var regjson = angular.toJson(registro);
+            deferred = $q.defer();
+            $http({
+                method: 'POST',
+                //skipAuthorization: true,
+                url: CONFIG.APISOSTOSBE + '/alumno/add',
+                data: regjson
+            }).then(function(res) {
+                deferred.resolve(res);
+            }).then(function(error){
+                deferred.reject(error);
+            })
+            return deferred.promise;
+        },
+        delAsi: function(id)
+        {
+            var regjson = angular.toJson(id);
+            deferred = $q.defer();
+            $http({
+                method: 'GET',
+                //skipAuthorization: true,
+                url: CONFIG.APISOSTOSBE + '/alumno/del/' + id
+            }).then(function(res) {
+                deferred.resolve(res);
+            }).then(function(error){
                 deferred.reject(error);
             })
             return deferred.promise;
         }
+
     }
 });
 
-app.controller('alumnosController', function ($scope, CONFIG, alumnosFact, ngDialog, $window, $rootScope) {
-  
-  var templateUpd =     'app/vmaintainer/preguntasfrecuente/updpreguntasfrecuente.htm';
-  $scope.templateAdd =  'app/vmaintainer/preguntasfrecuente/addpreguntasfrecuente.htm';
-  $scope.planilla = $scope.excel;
+app.controller('alumnosController', function ($scope, i18nService, CONFIG, apiAlumnoFactory, uiGridConstants) {
+
+  i18nService.setCurrentLang('es');
   $scope.gridOptions = {
+    enableRowSelection: true,
+    enableRowHeaderSelection: false,
+    enablePaginationControls: false,
+    multiSelect: false,
     enableSorting: true,
-      columnDefs: [
-          { field: 'name' },
-          { field: 'age' },
-          { field: 'location', enableSorting: false }
+    paginationPageSizes: [10, 30, 60],
+    paginationPageSize: 10,
+    columnDefs: [
+          { field: 'id', minWidth: 80, width: 80, enableColumnResizing: false },
+          { field: 'nombre_Asignatura', minWidth: 200, width: 300, enableColumnResizing: false },
+          { field: 'username', minWidth: 80, width: 110, enableColumnResizing: false },
+          { field: 'nombre', minWidth: 200, width: 250, enableColumnResizing: false },
+          { field: 'rut', minWidth: 200, width: 250, enableColumnResizing: false }
       ]
       ,onRegisterApi: function (gridApi) {
-      $scope.grid1Api = gridApi;
+      $scope.gridApi = gridApi;
       }
   };
-  
-    $scope.alumnos = null;
+  //$scope.gridOptions.columnDefs[1].visible = false;
+  //$scope.gridOptions.columnDefs[3].visible = false;
+  $scope.getAll = function () {
+      apiAlumnoFactory.getTodos().then(function (data) {
 
-  $scope.getAll = function ()
-  {
-    
-      //jQuery.support.cors = true;
-      $scope.alumnos = alumnosFact.get();
-      $scope.gridOptions.data = $scope.alumnos;
-      /*var lista2 = '{' + lista + '}';
-      //var lista = JSON.toString('{ data:' + alumnosFact.get() + '}');
-      var otable =  $('#tblalumnos').DataTable({
-         "paging": true,
-         "lengthChange": true,
-         "searching": true,
-         "ordering": true,
-         "info": true,
-         "autoWidth": false,
-         "processing": true,
-         "ajax": lista2,
-         "dataSrc": "",
-         "language": {"url":"//cdn.datatables.net/plug-ins/1.10.12/i18n/Spanish.json"},
-         "scrollY": "300px",
-         "pagingType": "full_numbers",
-         "columns": [
-                       { data : 'id' },
-                       { data : 'username'} ,
-                       { data : 'name'} ,
-                       { data : 'email'} ,
-                       { "targets": -1,
-                            "data": null,
-                            "defaultContent": "<button id='btnEditar'  class='btn btn-mini btn-primary'><i class='fa fa-pencil'></i> Editar</button> <button id='btnEliminar'  class='btn btn-mini btn-danger'><i class='fa fa-trash'></i> Eliminar</button>",
-                            "orderable": false }
-                       
-                    ],
-           "order": [[0, 'asc']]
-       });
+          $scope.gridOptions.data = data.data;
 
-
-      $('#tblalumnos' + ' tbody').on('click', 'button', function () {
-            var object = $(this);
-            var data = otable.row(object.parents('tr')).data();
-
-            if (object[0].id == 'btnEditar') {
-                  $scope.Dialogo(data, templateUpd);
-            }
-        });
-        */
+      }).then(function (data) {
+           $scope.getCombo();
+      });
   };
 
 
-  $scope.Dialogo = function(data, vtemplate)
-  {
-      ngDialog.open(
-                  {
-                      template: vtemplate,
-                      className: 'ngdialog-theme-default',
-                      data: JSON.stringify({ 'datos': data }),
-                      scope: $scope,
-                      showClose: false,
-                      overlay: true,
-                      closeByEscape: false,
-                      closeByDocument: false,
-                      id: 'dgAlumnos',
-                      width:'900px',
-                      height: '400px'
-                  });
+  $scope.editALU = function(){
+      var registro = $scope.gridApi.selection.getSelectedRows();
+      //$scope.getCombo();
+      if (registro != '') {
+          $scope.registroEdit = registro[0];
+      }
+  }
 
-  };
+
+  $scope.delALU = function(){
+      var registro = $scope.gridApi.selection.getSelectedRows();
+      if (registro != '') {
+          apiAlumnoFactory.delAlu(registro[0].id).then(function (data){
+            angular.forEach($scope.gridApi.selection.getSelectedRows(), function (data, index) {
+                $scope.gridOptions.data.splice($scope.gridOptions.data.lastIndexOf(data), 1);
+            });
+          })
+      } else {
+          alert('Debe seleccionar un registro');
+      }
+  }
+
+  $scope.updALU = function(registro){
+      apiAlumnoFactory.setAlu(registro).then(function (data) {
+          console.log(data.data);
+      })
+  }
+
+  $scope.addALU = function(registro){
+      apiAlumnoFactory.addAlu(registro).then(function (data) {
+          $scope.gridOptions.data.push(data.data);
+      })
+  }
 
 
 });
