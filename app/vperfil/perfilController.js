fe4c2c3bed2c89fc359c820490c2241e649d6507
cmarint@gmail.com <cmarint@gmail.com>
2017-08-17 10:20:09 -0300

    Actualiza perfil

--- app/vperfil/perfilController.js
+++ app/vperfil/perfilController.js
@@ -7,13 +7,22 @@ app.controller('perfilController', ['$scope','CONFIG', 'perfilFactory', '$http',
       });
     };
 
+    $scope.updPER = function (usuario) {
+      perfilFactory.updPer(usuario).then(function (data) {
+          //$scope.data = data.data;
+          console.log('Datos Actualizados')
+      }).then(function(error) {
+          console.log('Error:' + error);
+      });
+    };
+
 
 }])
 
 app.factory("perfilFactory", ["$http", "$q", "CONFIG", function($http, $q, CONFIG)
 {
 	return {
-		getPer: function(user)
+		getPer: function()
 		{
             var deferred;
             deferred = $q.defer();
@@ -21,6 +30,27 @@ app.factory("perfilFactory", ["$http", "$q", "CONFIG", function($http, $q, CONFI
                 method: 'GET',
                 skipAuthorization: false,
                 url: CONFIG.APISOSTOS +'/usuario/get/1',
+                data: user,
+                headers: {'Content-Type': 'application/json'}
+            })
+            .then(function(res)
+            {
+                deferred.resolve(res);
+            })
+            .then(function(error)
+            {
+                deferred.reject(error);
+            })
+            return deferred.promise;
+		},
+        updPer: function(user)
+		{
+            var deferred;
+            deferred = $q.defer();
+            $http({
+                method: 'POST',
+                skipAuthorization: false,
+                url: CONFIG.APISOSTOS +'/usuario/act',
                 data: user,
                 headers: {'Content-Type': 'application/json'}
             })
