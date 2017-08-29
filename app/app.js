2ebd1fefcd91dc3abee5e722a2dcfb83c501fdde
Claudio <cmarint@gmail.com>
2017-08-29 16:19:40 -0300

    $cookies

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ app.constant('CONFIG', {
     APISOSTOS: "http://168.232.165.85:8080/sostos_frontend_api"
 })
 
-app.run(['$rootScope','jwtHelper', 'store', '$location','$routeParams','$cookies',function($rootScope, jwtHelper, store, $location,$routeParams) {
+app.run(['$rootScope','jwtHelper', 'store', '$location','$routeParams','$cookies', function($rootScope, jwtHelper, store, $location,$routeParams,$cookies) {
 
    $rootScope.isUserLoggedIn = false ; //Cambiar a false
    //store.remove('token');
