b6baeaeeb56a7f852119df7ee975a14ec0d54f47
Claudio <cmarint@gmail.com>
2017-08-31 11:33:58 -0300

    Prueba logout

--- app/app.js
+++ app/app.js
@@ -188,7 +188,7 @@ app.config(function($routeProvider, $httpProvider, jwtInterceptorProvider, jwtOp
       }
  });
 
-app.controller('logoutController', function($scope, $rootScope, CONFIG, apiMenuFactory, $location, store, $cookies) {
+app.controller('logoutController', function($scope, $rootScope, CONFIG, apiMenuFactory, $location, store, $http, $cookies) {
           console.log('Saliendo......');
           store.remove('token');
           $cookies.remove('sostos.tkn');
