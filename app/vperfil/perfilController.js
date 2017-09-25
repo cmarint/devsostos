f356f6a5b6fc819d3ea6fcbd901c18e91465bfc7
Claudio Marin <cmarint@gmail.com>
2017-09-25 17:51:54 -0300

    actualizar preguntas seguridad

--- app/vperfil/perfilController.js
+++ app/vperfil/perfilController.js
@@ -10,22 +10,48 @@ app.controller('perfilController', ['$scope','CONFIG', 'perfilFactory', '$http',
     $scope.updPER = function (usuario) {
       perfilFactory.updPer(usuario).then(function (data) {
           //$scope.data = data.data;
-          console.log('Datos Actualizados')
+          console.log('Datos Actualizados');
       }).then(function(error) {
           console.log('Error:' + error);
       });
     };
 
+    $scope.updPRE = function (preg1, preg2) {
+      perfilFactory.setPreSeg(preg1).then(function (data) {
+          //$scope.data = data.data;
+          console.log('Datos Actualizados Preg1');
+      }).then(function(error) {
+          console.log('Error:' + error);
+      });
+
+       perfilFactory.setPreSeg(preg2).then(function (data) {
+          //$scope.data = data.data;
+          console.log('Datos Actualizados Preg2')
+      }).then(function(error) {
+          console.log('Error:' + error);
+      });
+    };
+
+
     $scope.getPRE = function () {
       perfilFactory.getPreSeg().then(function (data) {
-          $scope.data = data.data;
+          $scope.mispreg = data.data;
+      }).then(function (data) {
+          $scope.preg1 = $scope.mispreg[0];
+          $scope.preg2 = $scope.mispreg[1];
+      });
+    };
+
+    $scope.getPREALL = function () {
+      perfilFactory.getPregAll().then(function (data) {
+          $scope.listapreg = data.data;
       });
     };
 
 
 }])
 
-app.factory("perfilFactory", ["$http", "$q", "CONFIG", function($http, $q, CONFIG)
+app.factory("perfilFactory", ["$http", "$q", "CONFIG","$cookies", function($http, $q, CONFIG, $cookies)
 {
 	return {
 		getPer: function()
@@ -69,6 +95,21 @@ app.factory("perfilFactory", ["$http", "$q", "CONFIG", function($http, $q, CONFI
             })
             return deferred.promise;
 		},
+        getPregAll: function()
+        {
+            $http.defaults.headers.common.Authorization = 'Bearer ' + $cookies.get('sostos.tkn');
+            deferred = $q.defer();
+            $http({
+                method: 'GET',
+                skipAuthorization: true,
+                url: CONFIG.APISOSTOS + '/preguntaseguridad/get'
+            }).then(function(res) {
+                deferred.resolve(res);
+            }).then(function(error){
+                deferred.reject(error);
+            })
+            return deferred.promise;
+        },
         getPreSeg: function()
 		{
             var deferred;
@@ -90,6 +131,34 @@ app.factory("perfilFactory", ["$http", "$q", "CONFIG", function($http, $q, CONFI
             })
             return deferred.promise;
 		},
+        setPreSeg: function(obj)
+		{
+            var deferred;
+            var msg = {
+                    "estado": "",
+                    "respuesta": obj.resp,
+                    "id_Usuario": 0,
+                    "id_PreguntaSeguridad": obj.id_PreguntaSeguridad
+                }
+            $http.defaults.headers.common.Authorization = 'Bearer ' + $cookies.get('sostos.tkn');
+            deferred = $q.defer();
+            $http({
+                method: 'POST',
+                skipAuthorization: true,
+                url: CONFIG.APISOSTOS +'/usuario_preguntaseguridad/upd',
+                headers: {'Content-Type': 'application/json'},
+                data: msg
+            })
+            .then(function(res)
+            {
+                deferred.resolve(res);
+            })
+            .then(function(error)
+            {
+                deferred.reject(error);
+            })
+            return deferred.promise;
+		}
 
 	}
 }]);
