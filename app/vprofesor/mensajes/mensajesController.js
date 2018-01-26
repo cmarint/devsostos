06e4cbd9aefcb8b31698701c88bc80c5acf15eb6
Claudio Marin <cmarint@gmail.com>
2018-01-26 02:24:02 -0300

    Agrega servicio de mensaje

--- app/vprofesor/mensajes/mensajesController.js
+++ app/vprofesor/mensajes/mensajesController.js
@@ -15,16 +15,54 @@ app.factory('mensajesFactory', function($http, $q, CONFIG, store, $cookies){
                 deferred.reject(error);
             })
             return deferred.promise;
+        },
+        getMisAlumnos: function(id)
+        {
+            var url = CONFIG.APISOSTOS + '/profesor/asignatura/' + id + '/alumno/find';
+            $http.defaults.headers.common.Authorization = 'Bearer ' + $cookies.get('sostos.tkn');
+            return $http.post(url,{});
+        },
+        sendMail: function(obj)
+        {
+            $http.defaults.headers.common.Authorization = 'Bearer ' + $cookies.get('sostos.tkn');
+            deferred = $q.defer();
+            $http({
+                method: 'POST',
+                skipAuthorization: true,
+                url: CONFIG.APISOSTOS + '/mail/send',
+                data: obj
+            }).then(function(res) {
+                deferred.resolve(res);
+            }).then(function(error){
+                deferred.reject(error);
+            })
+            return deferred.promise;
         }
     }
 });
 
 app.controller('mensajesController', function ($scope, CONFIG, mensajesFactory, $filter, $location, $routeParams) {
 
-    $scope.getAsignaturas = function () {
-        mensajesFactory.getAsignatura().then(function (data) {
-            $scope.listaAsignatura = data.data.trxObject;
+    $scope.getAlumnos = function (id) {
+        mensajesFactory.getMisAlumnos(id).then(function (data) {
+            $scope.alumnos = data.data.trxObject;
         })
     }
-    console.log('Mensajes');
+
+    $scope.getAsignaturas = function () {
+        mensajesFactory.getAsignatura().then(function (data) {
+            $scope.combo = data.data.trxObject;
+        })
+    }
+
+    $scope.sendMail = function (obj) {
+                    var listadist = '';
+                    angular.forEach($scope.alumnos, function (value, key) {
+                             listadist = listadist + value.email + ","
+                    })
+        var test = { "toEmail": listadist, "subject": "[SOSTOS] - Informacion Importante", "body": obj.mensaje};
+        mensajesFactory.sendMail(test).then(function (data) {
+           console.log('Proband mail');
+        })
+    }
 });
