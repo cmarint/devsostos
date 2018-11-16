ad68f1361b1dbc065374d8cb5b0aad896f10ce01
Claudio Marín <iclaud@Claudios-MacBook-Pro.local>
2018-11-15 23:36:09 -0300

    debug

--- app/vmaintainer/alumnos/alumnosController.js
+++ app/vmaintainer/alumnos/alumnosController.js
@@ -95,7 +95,7 @@ app.controller('alumnosController', function ($scope, i18nService, CONFIG, apiAl
   $scope.comboCursoInstitucion = function () {
     apiAsignaturaFactory.getIns().then(function (data) {
         $scope.comboIns = data.data.trxObject;
-        console.log(data.data.trxObject);
+        console.log(data);
     });
   }
 
