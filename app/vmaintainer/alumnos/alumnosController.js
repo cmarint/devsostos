885f264232186e6e1ac88af6e7b9539dca6a8c84
Claudio Marín <iclaud@Claudios-MacBook-Pro.local>
2018-12-11 23:58:30 -0300

    Debug2

--- app/vmaintainer/alumnos/alumnosController.js
+++ app/vmaintainer/alumnos/alumnosController.js
@@ -111,7 +111,7 @@ app.controller('alumnosController', function ($scope, i18nService, CONFIG, apiAl
     for ( var i = 1; i < allTextLines.length; i++) {
         var data = allTextLines[i].split(',');
         var registro = { "nombre": data[0], "rut": data[1], "email": data[2], "estado": data[3]};
-        console.log("nombre:" + data[0]);
+        console.log(registro);
         /* $timeout( function(){
             apiAlumnoFactory.addAlumno(id, reg).then(function (data) {
                 console.log("id:" + id + " | registro:" + registro + " | data:" + data);
